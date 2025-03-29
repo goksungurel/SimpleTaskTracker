12da51af7d93ebdfa57781b48b4394db99a84e2f
goksungurel <148645864+goksungurel@users.noreply.github.com>
2025-03-29 23:44:01 +0300

    Update TaskManager.java

--- TaskManager.java
+++ TaskManager.java
@@ -1,4 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class TaskManager {
     ArrayList<Task> tasks=new ArrayList<>();
 
@@ -31,4 +37,39 @@ public class TaskManager {
             System.out.println("Task completed");
         }
     }
+  public void saveTasksToFile(String filename) {
+        try (FileWriter writer = new FileWriter(filename)) {
+            for (Task task : tasks) {
+                writer.write(task.getName() + ";" + task.isCompleted() + "\n");
+            }
+            System.out.println("Tasks saved to file");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving tasks.");
+            e.printStackTrace();
+        }
+    }
+
+    public void loadTasksFromFile(String filename) {
+        try(Scanner reader = new Scanner(new File(filename))) {
+            while(reader.hasNextLine()){
+                String line=reader.nextLine();
+                String[] tokens = line.split(";");
+                if(tokens.length == 2){
+                    String name = tokens[0];
+                    boolean completed = Boolean.parseBoolean(tokens[1]);
+
+                    Task task = new Task(name);
+                    task.setCompleted(completed);
+                    addTask(task);
+                }
+
+            }
+            System.out.println("Tasks loaded from file");
+        }catch (FileNotFoundException e) {
+            System.out.println("No saved file found. Starting with an empty task list.");
+        }
+
+    }
 }
+
