95c007d02e72b2c10b8d753bd01014b760d88548
goksungurel <148645864+goksungurel@users.noreply.github.com>
2025-03-29 23:19:00 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        TaskManager manager = new TaskManager();
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\n=== TASK TRACKER SYSTEM ===");
+            System.out.println("1. Add Task");
+            System.out.println("2. List Tasks");
+            System.out.println("3. Complete Task");
+            System.out.println("4. Remove Task");
+            System.out.println("5. Exit");
+            System.out.print("Your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter Task Name: ");
+                    String taskName = scanner.nextLine();
+                    Task newTask = new Task(taskName);
+                    manager.addTask(newTask);
+                    System.out.println("Task added successfully.");
+                    break;
+                case 2:
+                    manager.listTasks();
+                    break;
+                case 3:
+                    System.out.println("Enter  task number to complete:  ");
+                    int index=scanner.nextInt() -1;
+                    manager.completeTask(index);
+                    System.out.println("Task completed successfully.");
+                    break;
+
+                case 4:
+                    System.out.println("Enter  task number to remove:  ");
+                    int indexnum=scanner.nextInt() -1;
+                    manager.removeTask(indexnum);
+                    System.out.println("Task removed successfully.");
+                    break;
+
+                case 5:
+                    running = false;
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+
+        }
+        scanner.close();
+
+
+    }
+}
